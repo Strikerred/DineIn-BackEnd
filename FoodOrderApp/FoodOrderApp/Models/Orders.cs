81363c2868fcd1fa04f4ac4163e829f78b52cfab
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-01 01:04:23 -0700

    Add Database models

---
+++ FoodOrderApp/FoodOrderApp/Models/Orders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderApp.Models
+{
+    public class Orders
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+        [Required]
+        [ForeignKey("MenuItemId")]
+        public virtual MenuItems MenuItemId { get; set; }
+        [Required]
+        [ForeignKey("UserId")]
+        public virtual Users Users { get; set; }
+
+    }
+}
---
+++ FoodOrderApp/FoodOrderApp/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderApp.Models
+{
+    public class Users
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [Required]
+        public String Email { get; set; }
+        [Required]
+        public String Password { get; set; }
+        public bool RememberMe { get; set; }
+        public DateTime EmailVerifiedAt { get; set; }
+        [Required]
+        public String PhoneNumber { get; set; }
+        [Required]
+        public String Address { get; set; }
+    }
+}
