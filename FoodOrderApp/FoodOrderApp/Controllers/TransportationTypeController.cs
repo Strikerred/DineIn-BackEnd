46246c7241527ef85436096bc5075b77efb7d592
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-05 16:10:17 -0700

    Get All - TransportationType

--- FoodOrderApp/FoodOrderApp/Controllers/TransportationTypeController.cs
+++ FoodOrderApp/FoodOrderApp/Controllers/TransportationTypeController.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodOrderApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodOrderApp.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
     public class TransportationTypeController : Controller
     {
-        
+        private sqlContext _context;
+
+        public TransportationTypeController(sqlContext context)
+        {
+            _context = context;
+        }
+
+        // TransportationTypes Data
+        // show all Transportation types
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TransportationType>>> GetTransportationTypes()
+        {
+            return await _context.TransportationType.ToListAsync();
+        }
     }
 }
