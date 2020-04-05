a4cb1d4c752ed1753eb7ab8d9b637b729d0c58dd
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-04 20:38:22 -0700

    Initial create customer Controller

---
+++ FoodOrderApp/FoodOrderApp/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrderApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        // Customer Data
+
+
+        // Transportation types
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
