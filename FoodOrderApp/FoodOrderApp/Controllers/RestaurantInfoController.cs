38429b5aa4a5f423c27caa337c7ce330f4f88a03
Fausto Gomez <gomez_fausto@hotmail.com>
2020-04-05 00:09:04 -0700

    added auth required for add and put a restaurant.

--- FoodOrderApp/FoodOrderApp/Controllers/RestaurantInfoController.cs
+++ FoodOrderApp/FoodOrderApp/Controllers/RestaurantInfoController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FoodOrderApp.Data;
 using FoodOrderApp.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
 namespace FoodOrderApp.Controllers
 {
     [Route("api/[controller]")]
@@ -39,6 +42,7 @@ namespace FoodOrderApp.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> PutRestaurantInfo(int id, RestaurantInfo restaurantInfo)
         {
             if (id != restaurantInfo.RestaurantId)
@@ -67,6 +71,7 @@ namespace FoodOrderApp.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<ActionResult<RestaurantInfo>> PostRestaurantInfo(RestaurantInfo restaurantInfo)
         {
             _context.RestaurantInfo.Add(restaurantInfo);
