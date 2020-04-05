c205f880ed9e717aa3b481af19afcc3260c89b78
gcarpenter7@my.bcit.ca <gcarpenter7@my.bcit.ca>
2020-04-05 15:24:23 -0700

    Added put method; not working correctly

--- FoodOrderApp/FoodOrderApp/Controllers/CustomerController.cs
+++ FoodOrderApp/FoodOrderApp/Controllers/CustomerController.cs
@@ -10,11 +10,11 @@ using FoodOrderApp.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 
-
 namespace FoodOrderApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class CustomerController : Controller
     {
         private sqlContext _context;
@@ -31,16 +31,44 @@ namespace FoodOrderApp.Controllers
         {
             return await _context.CustomerInfo.ToListAsync();
         }
-
+        // Get one Customer object by id
         [HttpGet("{id}")]
-        public async Task<ActionResult<CustomerInfo>>GetCustomer(int id)
+        public async Task<ActionResult<CustomerInfo>> GetCustomer(int id)
         {
             var oneCustomer = await _context.CustomerInfo.FindAsync(id);
-            if(oneCustomer == null)
+            if (oneCustomer == null)
             {
                 return NotFound();
             }
             return oneCustomer;
+        }
+
+        //Update one customer object by id
+        [HttpPut("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> UpdateCustomerInfo(int id, CustomerInfo customerInfo)
+        {
+            if(id != customerInfo.CustomerId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(customerInfo).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if(!CustomerInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
         }
 
         // Transportation types
