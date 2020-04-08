2abfe3aa9981c88d60406e394b6f3dd78b7b34ca
Fausto Gomez <gomez_fausto@hotmail.com>
2020-04-07 17:39:25 -0700

    ItemSelected feature added to store more than one items, and order was modified based on itemSelected feature.

---
+++ FoodOrderApp/FoodOrderApp/Models/ItemSelected.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderApp.Models
+{
+    public class ItemSelected
+    {
+        public int ItemSelectedId { get; set; }
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+
+        public virtual Orders Orders { get; set; }
+    }
+}
